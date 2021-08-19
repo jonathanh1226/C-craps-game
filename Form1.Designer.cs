af3613f034d8b7e556d236a1825cfaceae422481
jonathanh1226 <88116200+jonathanh1226@users.noreply.github.com>
2021-08-19 11:37:51 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Craps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblX = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRoll.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRoll.Location = new System.Drawing.Point(131, 212);
+            this.btnRoll.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(88, 37);
+            this.btnRoll.TabIndex = 0;
+            this.btnRoll.Text = "Roll Dice";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(159, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(337, 11);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 65);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(199, 172);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 20);
+            this.lblResult.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(337, 212);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX.Location = new System.Drawing.Point(219, 128);
+            this.lblX.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(0, 18);
+            this.lblX.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRoll);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblX;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Craps
+{
+    public partial class Form1 : Form
+    {
+        Image img1, img2;
+        int dice1, dice2;
+        int Sum = 0;
+        bool attempt = true;
+        int attempt2 = 0;
+        int result1 = 0;
+        int result2 = 0;
+        Random rnd = new Random();
+        String lose = "YOU LOSE";
+        String win = "YOU WIN!";
+        String draw = "YOU DRAW";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+        }
+        private void getRandom()
+        {
+            result1 = rnd.Next(1,7);
+            result2 = rnd.Next(1,7);
+            Sum = (result1 + result2);
+
+        }
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            //Reads from the "Roll Dice" class
+            playGame();
+          
+
+        }
+        private void playGame()
+        {
+            getRandom();
+            if(attempt)
+            {
+                RollDie();
+            }
+            else
+            {
+                RollDie0();
+            }
+        }
+        private void RollDie()
+        {
+            if (result1 == 1)
+            {
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 1.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result1 == 2)
+            {
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 2.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result1 == 3)
+            {
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 3.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result1 == 4)
+            {
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 4.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result1 == 5)
+            {
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 5.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else
+            {
+
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 6.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            }
+            //picturebox 2 and dice 2 
+            if (result2 == 1)
+            {
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 1.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result2 == 2)
+            {
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 2.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result2 == 3)
+            {
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 3.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result2 == 4)
+            {
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 4.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result2== 5)
+            {
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 5.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else
+            {
+
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 6.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            }
+            switch (Sum)
+            {
+            
+                 case 2:
+                 case 3:
+                 case 12:
+                     {
+                     
+                         DisplayL();
+                         break;
+                     }
+                 case 7:
+                 case 11:
+                     {
+                         DisplayW();
+                         break;
+                     }
+                 case 4:
+                 case 5:
+                 case 6:
+                 case 8:
+                 case 9:
+                 case 10:
+                    {
+                        DisplayD();
+                        attempt2 = Sum;
+                        break;
+                    }
+
+            }
+            attempt = false;
+        }
+        private int getX()
+        {
+            return attempt2;
+        }
+        private void RollDie0()
+        {
+            getX();
+            int X = attempt2;
+            lblX.Text=("Number to hit Again:" + X);
+            getRandom();
+            
+            if (result1 == 1)
+            {
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 1.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result1 == 2)
+            {
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 2.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result1 == 3)
+            {
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 3.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result1 == 4)
+            {
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 4.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result1 == 5)
+            {
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 5.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else
+            {
+
+                img1 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 6.png");
+
+                pictureBox1.Image = img1;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            }
+            //picturebox 2 and dice 2 
+            if (result2 == 1)
+            {
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 1.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result2 == 2)
+            {
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 2.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result2 == 3)
+            {
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 3.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result2 == 4)
+            {
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 4.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else if (result2 == 5)
+            {
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 5.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else
+            {
+
+                img2 = Image.FromFile(@"C:\Users\jonat\OneDrive\Desktop\c#\C#Project_JHernandez\Craps\Craps\Resources\dice 6.png");
+
+                pictureBox2.Image = img2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            }
+            if (Sum == X)
+            {
+                DisplayW();
+                lblX.Text = ("Number to hit Again:" + X);
+            }
+           else
+            {
+                DisplayD();
+            }
+           if(Sum == 7)
+            {
+                DisplayL();
+                lblX.Text = ("Number to hit Again:" + X);
+
+            }
+        }
+
+        private void DisplayL()
+        {
+            lblResult.Text=($"{lose} You Draw {Sum}");
+            Reset();
+
+        }
+        private void DisplayD()
+        {
+            lblResult.Text=($"{draw} {Sum},ROLL AGAIN");
+        }
+        private void DisplayW()
+        {
+            lblResult.Text=($"{win} You Draw {Sum}");
+            Reset();
+        }
+        private void Display()
+        {
+            lblResult.Text=($"You rolled {Sum}");
+        }
+
+        private void Reset()
+        {
+             Sum = 0;
+             attempt = true;
+             attempt2 = 0;
+             result1 = 0;
+             result2 = 0;
+            btnRoll.Enabled = false;
+             lblX.Text = " ";
+        }
+
+    
+      private  void ResetResults()
+        {
+            lblResult.Text = "";
+            pictureBox1.Image = Properties.Resources.blank;
+            pictureBox2.Image = Properties.Resources.blank;
+            pictureBox2.Visible = true;
+            Sum = 0;
+            btnRoll.Enabled = true;
+            Sum = 0;
+            attempt = true;
+            attempt2 = 0;
+            result1 = 0;
+            result2 = 0;
+            lblX.Text = " ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResetResults();
+        }
+    }
+}
